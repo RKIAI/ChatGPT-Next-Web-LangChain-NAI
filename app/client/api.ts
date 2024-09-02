a2e228ef58a59b957af9cdc0a359ab3de2303d98
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-09-02 17:12:45 +0500

    Update api.ts

--- app/client/api.ts
+++ app/client/api.ts
@@ -197,7 +197,7 @@ export class ClientApi {
         {
           from: "human",
           value:
-            "Share from [GPT-CHATBOT.ru]: https://gpt-chatbot.ru",
+            "Share from [NiansuhAI]: https://typegpt.net",
         },
       ]);
     // 敬告二开开发者们，为了开源大模型的发展，请不要修改上述消息，此消息用于后续数据清洗使用
