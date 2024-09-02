f89f39ac771924fcdfe5c5aed0c5cf20989c01da
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-09-02 17:25:34 +0500

    Update update.ts

--- app/store/update.ts
+++ app/store/update.ts
@@ -106,7 +106,7 @@ export const useUpdateStore = createPersistStore(
                       if (version === remoteId) {
                         // Show a notification using Tauri
                         window.__TAURI__?.notification.sendNotification({
-                          title: "GPT-CHATBOT.ru",
+                          title: "TypeGPT",
                           body: `${Locale.Settings.Update.IsLatest}`,
                           icon: `${ChatGptIcon.src}`,
                           sound: "Default",
@@ -116,7 +116,7 @@ export const useUpdateStore = createPersistStore(
                           Locale.Settings.Update.FoundUpdate(`${remoteId}`);
                         // Show a notification for the new version using Tauri
                         window.__TAURI__?.notification.sendNotification({
-                          title: "GPT-CHATBOT.ru",
+                          title: "TypeGPT",
                           body: updateMessage,
                           icon: `${ChatGptIcon.src}`,
                           sound: "Default",
